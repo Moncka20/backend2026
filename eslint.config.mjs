255a7c28af7fd08f98cce2611c16c0ee189a1429
ThomasMoncayo <tamoncayo24@itp.edu.co>
2026-04-14 20:56:20 -0500

    feat: add categoria, inventario, rol, and usuario modules with basic CRUD functionality
    - Implemented Categoria module with service, controller, and entity.
    - Created Inventario module with service, controller, and entity.
    - Added Rol module with service, controller, and entity for role management.
    - Developed Usuario module with service, controller, and entity for user management.
    - Introduced DTOs for create and update operations in Categoria, Inventario, and Producto.
    - Integrated TypeORM for database interactions and established relationships between entities.
    - Set up Swagger documentation for API endpoints.
    - Added global validation and serialization interceptors in main application bootstrap.
    - Created initial tests for services and controllers in each module.

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      sourceType: 'commonjs',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
